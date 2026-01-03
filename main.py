8b4a13090998c24c74fc0572baa6aeac33d90326
Thato <Nkadimeng.Thato@icloud.com>
2026-01-03 12:40:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello World!")
+
+# Printing my name
+print("My name is Thato")
+
+#printing a fun fact about myself
+print("I love reading and doing origami")
+
+# Performing and printing a simple calcualation
+print("The answer to 6 + 7 is ", 6 + 7)
